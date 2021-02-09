1c6de40d60a6a5c35c7c2069d5a9ac8a6e8f08e7
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-02-09 13:49:18 +0300

    added .env

--- conf.js
+++ conf.js
@@ -1,10 +1,11 @@
-const mysql = require('mysql');
+require("dotenv").config();
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'yourPassword',
-  database: 'yourDatabaseName'
+  host: "localhost",
+  user: "root",
+  password: `${process.env.MYSQL_PASSWORD}`, //drM.
+  database: `${process.env.MYSQL_DATABASE}`, //remote
 });
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express'); // we require the express package
-const connection = require('./conf')
+const express = require("express"); // we require the express package
+const connection = require("./conf");
 const app = express(); // we store it in a variable named app, all methods coming from express
-const cors = require('cors'); // we require the Cors package
-const port = 5000; // we set our backend port so our server will be at localhost:5000
+const cors = require("cors"); // we require the Cors package
+const port = 6000; // we set our backend port so our server will be at localhost:5000
 
 // we need to tell our server to accept and be ok with strings coming as request and convert it to JSON objects
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // we use cors package, by default, it will allow all upcoming request to avoid any cors policy
@@ -13,57 +13,82 @@ app.use(cors());
 
 // connection to the DB
 connection.connect(function (err) {
-  if(err) {
-    console.error('error trying to connect to the DB. Error: ' + err.stack);
+  if (err) {
+    console.error("error trying to connect to the DB. Error: " + err.stack);
     return;
   }
-  console.log('Succesfuly connected to the DB with the ID: ' + connection.threadId)
-})
+  console.log(
+    "Successfully connected to the DB with the ID: " + connection.threadId
+  );
+});
 
-
+// simulation of a DB
+// const students = [
+//   {
+//     name: 'Marc',
+//     age: 20
+//   },
+//   {
+//     name: 'Dean',
+//     age: 23
+//   },
+//   {
+//     name: 'Ioan',
+//     age: 21
+//   },
+//   {
+//     name: 'Naomi',
+//     age: 34
+//   },
+//   {
+//     name: 'Teiko',
+//     age: 3434
+//   }
+// ]
 
 // on the localhost:5000, we are taking care of the response to send a string
-app.get('/', (req, res) => {
-  res.send('hello from the server!')
-})
+app.get("/", (req, res) => {
+  res.send("hello from the server!");
+});
 
 // on localhost:5000/students we will send, as a json, the whole students object
-app.get('/students', (req,res) => {
-
+app.get("/students", (req, res) => {
   // Connect to the DB, do a query to get everybody and send it to the FE
-  connection.query('SELECT * FROM students', (err, results) => {
-    if(err) {
-      res.status(500).send('Server error, could not fetch students')
+  connection.query("SELECT * FROM students", (err, results) => {
+    if (err) {
+      res.status(500).send("Server error, could not fetch students");
     } else {
-      res.json(results)
+      res.json(results);
     }
-  })
-
-})
+  });
+});
 
 // we can use the same /students endpoint, to do handle all the POST requests
-app.post('/students', (req, res) => {
+app.post("/students", (req, res) => {
   // just store what's coming from the body of the request in a variable to be easy reusable
   let student = {
     user: req.body.name,
-    age: req.body.age
-  }
+    age: req.body.age,
+  };
+  //THE BODY IN POSTMAN SHOULD LOOK LIKE:
+  // {
+  //   "name": "Tommy",
+  //   "age":"20"
+  // }
+
   // connection to the DB, we will insert into students whatever is coming in the student object we've just created
   // the SET method will add in our table all the fields related to that object
-  connection.query('INSERT INTO students SET ?', student, (err) => {
-    if(err) {
-      res.status(500).send('Error, your post request did not succeed')
+  connection.query("INSERT INTO students SET ?", student, (err) => {
+    if (err) {
+      res.status(500).send("Error, your post request did not succeed");
     } else {
-      res.sendStatus(200)
+      res.sendStatus(200);
     }
-  })
-})
-
-
-
+  });
+});
 
 // makes our BE server listen to the port and throw any possible error
-app.listen(port, err => {
-  if(err) throw new Error('Something is not working, sorry :( ...')
-  console.log(`Server is running amazingly on port ${port}`)
-})
+app.listen(port, (err) => {
+  if (err) throw new Error("Something is not working, sorry :( ...");
+  console.log(`Server is running amazingly on port ${port}`);
+});
